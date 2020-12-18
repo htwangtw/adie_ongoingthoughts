7ae08b18a1f3e6fa97ac27006d6fcfea1ed9dc15
Hao-Ting Wang <htwangtw@gmail.com>
2020-12-18 15:19:40 +0000

    [ENH] clean subject summary

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # ADIE ongoing thought analysis
 
 Authors: Hao-Ting Wang, Will Strawson
+
+## setup
+Environment for this project uses Python >= 3.8.
+```
+virtualenv env -p /path/to/bin/python3.8
+pip install -i requirements.txt
+```
+For developers:
+```
+virtualenv env -p /path/to/bin/python3.8
+pip install -i dev-requirements.txt
+```
--- data/code/beh_BIDS.py
+++ data/code/beh_BIDS.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pandas as pd
+
 from adie.dataset import *
 from adie.spike import smr2array
 
@@ -8,12 +10,14 @@ sourcedata = "projects/adie_ongoingthoughts/data/sourcedata/nback_mindwadnering"
 sourcedata = Path.home() / sourcedata
 
 # create a dir for all subject id present
+participant_info = []
 for cur_file in sourcedata.glob("**/*mindwandering_MDES*_data.csv"):
     info = cur_file.parent.name
     sub, session, group = parseinfo(info)
     sub_path, base_name = gen_bidsbeh(bids_home, sub, session)
     target = convert_beh(cur_file, sub_path,
                          base_name, "nbackexpsampling")
+
 
 # do the same for smr files
 for cur_file in sourcedata.glob("**/*.smr"):
@@ -24,3 +28,21 @@ for cur_file in sourcedata.glob("**/*.smr"):
     _ = save_physio(sub_path, bidsnames, signal_info, signals)
 
 
+# compile summary
+participant_info = []
+for p in bids_home.glob("sub-*/ses-*/"):
+    ses = p.name.split("-")[-1]
+    sub = p.parent.name.split("-")[-1]
+    group = "control" if sub[0] == "C" else "patient"
+
+    task_exist = len(list(p.glob("beh/*nbackexpsampling*"))) > 0
+    physio_exist = len(list(p.glob("beh/*ecg*"))) > 0
+
+    participant_info.append({"particpant_id": sub,
+                             "session": ses,
+                             "group": group,
+                             "task_nbackexpsampling": int(task_exist),
+                             "physio_nbackexpsampling": int(physio_exist)})
+
+participant_info = pd.DataFrame(participant_info, index=None)
+participant_info.to_csv(bids_home / "participants.tsv", index=False, sep="\t")
