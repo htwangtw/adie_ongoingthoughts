d1e7a4e432e1c5b37e3ce4d58f88e0a2b0069906
Hao-Ting Wang <htwangtw@gmail.com>
2020-12-18 16:29:19 +0000

    [ENH] python 3.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     author="Hao-Ting Wang",
     author_email='htwangtw@gmail.com',
-    python_requires='>=3.8',
+    python_requires='>=3.7',
     description="adie ongoing tought analysis tools",
     name='adie',
     packages=find_packages(),
