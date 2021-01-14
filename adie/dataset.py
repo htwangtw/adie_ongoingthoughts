46d5f87be50a0a6f1e53be03116839d817bb74a5
Hao-Ting Wang <htwangtw@gmail.com>
2021-01-14 16:27:58 +0000

    add generator of spike 2 derivative in BIDS

--- adie/dataset.py
+++ adie/dataset.py
@@ -1,5 +1,6 @@
 import re
 import os
+from shutil import copyfile
 import json
 
 from pathlib import Path
@@ -23,10 +24,13 @@ def parseinfo(info: str) -> (str, str, str):
     input
     -----
     info: str
+        ADIE participant number
 
     output
     ------
      sub, session, group: str
+        BIDS compatable ADIE information
+
     """
     p = re.search("((CON)?ADIE[0-9]*)(_)?([A-Z]*)", info)
     sub = p.group(1)
@@ -36,22 +40,46 @@ def parseinfo(info: str) -> (str, str, str):
 
 
 def gen_bidsbeh(bidsroot: Path or str,
-                sub: str, session: str) -> (Path, str):
+                sub: str, session: str = None,
+                derivative: str = None) -> (Path, str):
     """
     Generate behavioural data file path
+
+    input
+    -----
+    bidsroot:
+        BIDS directory
+    sub:
+        subject ID
+    session:
+        session label
+    derivative:
+        BIDS derivative name (default None)
+
+    output
+    ------
+    new_sub, base_name: str
+        BIDS subject directory and BIDS subject file base name
     """
 
     if type(bidsroot) ==str:
         bidsroot = Path(bidsroot)
-    base_name = f"sub-{sub}_ses-{session}"
-    dir_template = f"sub-{sub}/ses-{session}/beh"
+
+    if session:
+        base_name = f"sub-{sub}_ses-{session}"
+        dir_template = f"sub-{sub}/ses-{session}/beh"
+    else:
+        base_name = f"sub-{sub}"
+        dir_template = f"sub-{sub}/beh"
+
+    if derivative:
+        dir_template = f"derivatives/{derivative}/{dir_template}"
+
     new_sub = Path(bidsroot / dir_template)
     if new_sub.is_dir():
-        print(f"""behavioural data directory exist:
-sub-{sub}, ses-{session}""")
+        print(f"""behavioural data directory exist: {base_name}""")
     else:
-        print(f"""behavioural data directory created:
-sub-{sub}, ses-{session}""")
+        print(f"""behavioural data directory created: {base_name}""")
         os.makedirs(new_sub)
     return new_sub, base_name
 
@@ -66,16 +94,33 @@ def convert_beh(original: Path, target: Path,
     return target / target_file
 
 
-def name_physiobids(basename: str, label: str, signal_info: list) -> list:
+def name_physiobids(basename: str, task: str, signal_info: list) -> list:
+    """
+    generate physiology data name
+
+    input
+    -----
+    basename:
+        BIDS subject file base name
+    task:
+        task label
+    signal_info:
+        list of json containing meta data of the signal
+
+    output
+    ------
+    names:
+        list of physio data name
+    """
     names = []
     for d in signal_info:
         recording = d["Columns"][0]
         if recording in physio_labels:
             suffix = "physio"
-            physio_basename = f"{basename}_task-{label}_recording-{recording}_{suffix}"
+            physio_basename = f"{basename}_task-{task}_recording-{recording}_{suffix}"
         else:
             suffix = "stim"
-            physio_basename = f"{basename}_task-{label}_{suffix}"
+            physio_basename = f"{basename}_task-{task}_{suffix}"
         names.append(physio_basename)
     return names
 
@@ -84,6 +129,23 @@ def save_physio(target: Path, bidsnames: list,
                 signal_info: list, signals: list) -> list:
     """
     save converted spike physio data to BIDS spec beh file
+
+    input
+    -----
+    target:
+        target directory
+    bidsnames:
+        bids compatable file name
+    signal_info:
+        list of json containing meta data of the signal
+
+    signals:
+        list containing the signal in 1d numpy arrays
+
+    output
+    ------
+    names:
+        list of saved physio data path
     """
     saved = []
     for n, d, s in zip(bidsnames, signal_info, signals):
@@ -94,3 +156,30 @@ def save_physio(target: Path, bidsnames: list,
         saved.append(str(target / f"{n}"))
 
     return saved
+
+def smr_derivative(origin: Path, target: Path,
+                   basename: str, task: str, recording: str) -> Path:
+    """
+    organise the smr files in BIDS for people who
+    prefer to use original spike files
+
+    input
+    -----
+    origin:
+        path to the smr file
+    target:
+        path to the derivative directory
+    basename:
+        BIDS basename
+    task:
+        associated task name
+    recording:
+        optional, type of recording
+
+    output
+        path to the renamed file
+    ------
+    """
+    target_file = f"{basename}_task-{task}_recording-{recording}_physio.smr"
+    copyfile(origin, target / target_file)
+    return target / target_file
--- test/dataset_test.py
+++ test/dataset_test.py
@@ -42,15 +42,27 @@ def test_pasreinfo():
 
 def test_gen_bidsbeh(tmpdir):
     print("check template")
-    path, bn = gen_bidsbeh(bidsroot, "ADIE983", "baseline")
+    path, bn = gen_bidsbeh(bidsroot, "ADIE983", session="baseline")
     assert path == Path(bidsroot) / "sub-ADIE983/ses-baseline/beh"
     assert path.is_dir() is True
     assert bn == "sub-ADIE983_ses-baseline"
+
+    print("single session")
+    path, bn = gen_bidsbeh(bidsroot, "ADIE983")
+    assert path == Path(bidsroot) / "sub-ADIE983/beh"
+    assert path.is_dir() is True
+    assert bn == "sub-ADIE983"
 
     print("check file creation")
     path, bn = gen_bidsbeh(tmpdir, "ADIE983", "baseline")
     assert bn == "sub-ADIE983_ses-baseline"
     assert path == tmpdir / "sub-ADIE983/ses-baseline/beh"
+
+    print("derivative, no session")
+    path, bn = gen_bidsbeh(bidsroot, "ADIE983", derivative="mytask")
+    assert path == Path(bidsroot) / "derivatives/mytask/sub-ADIE983/beh"
+    assert path.is_dir() is True
+    assert bn == "sub-ADIE983"
 
 def test_conver_beh(tmpdir):
     origin = bidsroot / "sourcedata/ADIE983_beh_task.csv"
@@ -73,3 +85,11 @@ def test_save_physio(tmpdir):
     assert len(saved) == len(bidsnames)
     assert saved[0] == str(tmpdir / bidsnames[0])
     assert saved[1] == str(tmpdir / bidsnames[1])
+
+def test_smr_derivative(tmpdir):
+    test_file = Path(__file__).parent /  "data/file.smr"
+    sub_path, bn = gen_bidsbeh(tmpdir, "ADIE983", derivative="mytask_smr")
+    saved = smr_derivative(test_file, sub_path,
+                           basename=bn,
+                           task="mytask", recording="ecg")
+    assert saved == sub_path / f"{bn}_task-mytask_recording-ecg_physio.smr"
