4bfe1952a9ab59d938581a90520f50160b0586a4
Hao-Ting Wang <htwangtw@gmail.com>
2021-01-15 23:11:08 +0000

    ENH nback function error catching

--- adie/nback.py
+++ adie/nback.py
@@ -1,7 +1,12 @@
+from numpy.core.numeric import identity
 import pandas as pd
 from pathlib import Path
 
 from .bids import parse_bids_filename, update_entity
+
+
+accept_stimtype = ["MWQ", "NT", "TT", "END"]
+identity_entity = ["sub", "ses", "run"]
 
 def read_log(filepath: Path) -> dict:
     '''
@@ -14,10 +19,13 @@ def extract_stimtype(data: pd.DataFrame, filename: str,
     """
     Get trials with matching label under stimType
     """
+    if stimtype not in accept_stimtype:
+        raise ValueError(f"invalid {stimtype}, only accept {accept_stimtype}")
+
     parsed_fn = parse_bids_filename(filename)
     stimresp = data.query(f"stimType == '{stimtype}'")
     sliced = stimresp.loc[:, columns]
-    return update_entity(sliced, ["sub", "ses", "run"], parsed_fn)
+    return update_entity(sliced, identity_entity, parsed_fn)
 
 def extract_performance(self, parameter_list):
     """
