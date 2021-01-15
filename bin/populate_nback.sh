e89c3303450ef7fca741726638c8d06aa16ba37c
Hao-Ting Wang <htwangtw@gmail.com>
2021-01-15 00:45:38 +0000

    cluster file

--- bin/populate_nback.sh
+++ bin/populate_nback.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+#$ -N populate_nback
+#$ -o /home/$USER/logs
+
 TASK=$1
 BIDSDIR=$2
+
+cd ${BIDSDIR}/code
+source env/bin activate
+
+pip install ${BIDSDIR}/code/
 
 shopt -s globstar
 subid=`ls ${BIDSDIR}/sourcedata/${TASK}/**/*ADIE*/ -d | awk -F "/" '{printf("%s\n", $(NF-1)); }'`
