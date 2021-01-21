444f19a48a0b92f8a6dea7b9a4d7fdc7a0bbadc1
JoelPatchitt <77615720+JoelPatchitt@users.noreply.github.com>
2021-01-21 11:39:00 +0000

    Update generate_participant_splitpath.py

--- bin/generate_participant_splitpath.py
+++ bin/generate_participant_splitpath.py
@@ -1,27 +1,23 @@
 from pathlib import Path
-Path.cwd()
+import os
+import pandas as pd
+
+
 data = Path.cwd()
-data.absolute() # Prints absolute path
-data.glob("Sub_*") # Selects all directories named Sub_* In Python, the glob module is used to retrieve files/pathnames matching a specified pattern.
-subj=list(data.glob("Sub_*")) # Lists directories
-
-sub_str = [ str(e) for e in subj] # subjects as string, converts elements from windows path to string retaining list format
-
-import os  
-len(sub_str) # Equals amount of elements in substr list
+subj = list(data.glob("Sub_*")) # Lists directories
+sub_str = [str(e) for e in subj] # Subjects as string, converts elements from windows path to string retaining list format 
 total_elements = len(sub_str) # Variable for while loop
 element = 0 # List elements start at 0
 
-while element <= total_elements-1: # total_elements = 4 in this instance
+while element <= total_elements - 1: # Total_elements = 4 in this instance
 
     headtail = os.path.split(sub_str[element])
     sub_str[element] = headtail[1]
     
-    element = element+1 #End of loop
+    element = element + 1 #End of loop
 
 #Convert list into a a dateframe
-from pandas import DataFrame
-df = DataFrame (sub_str,columns=['Subjects'])
+df = pd.DataFrame(sub_str,columns=['Subjects'])
 print (df)
 
-df.to_csv('Participant_generator2.tsv', sep = '\t', index=False) # Output to .tsv file
+df.to_csv('Participant_generator.tsv', sep='\t', index=False) # Output to .tsv file
