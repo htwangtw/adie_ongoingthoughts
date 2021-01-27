a4d0435d3bc8f1c18d1bd34693b1fa2e864609a5
JoelPatchitt <77615720+JoelPatchitt@users.noreply.github.com>
2021-01-27 09:48:47 +0000

    Update generate_participant.py

--- bin/generate_participant.py
+++ bin/generate_participant.py
@@ -7,18 +7,17 @@ import pandas as pd
 #bids_dir = Path(get_test_data_path())
 #subj = list(bids_dir.glob("sub-*"))
 
-data = Path.cwd()
-subj = list(data.glob("Sub_*")) # Lists directories
-sub_str = [str(e) for e in subj] # Subjects as string, converts elements from windows path to string retaining list format 
+def generate_participants(datafile):
+    subj = list(datafile.glob("Sub-*")) # Lists directories
+    sub_str = [str(e) for e in subj] # Subjects as string, converts elements from windows path to string retaining list format 
 
-sub_id = []
-while sub_str:  # stop when sub_str is empty
-    cur_sub = sub_str.pop()  # pop an item from the list
-    cur_sub = cur_sub.split(os.sep) # split string by os specific separator, return a list of strings
-    sub_id.append(cur_sub[-1]) # save output
+    sub_id = []
+    while sub_str:  # stop when sub_str is empty
+        cur_sub = sub_str.pop()  # pop an item from the list
+        cur_sub = cur_sub.split(os.sep) # split string by os specific separator, return a list of strings
+        sub_id.append(cur_sub[-1]) # save output
 
-#Convert list into a a dateframe
-df = pd.DataFrame(sub_id,columns=['participant_id'])
-print (df)
+    #Convert list into a a dateframe
+    df = pd.DataFrame(sub_id,columns=['participant_id'])
 
 df.to_csv('participants.tsv', sep='\t', index=False) # Output to .tsv file
