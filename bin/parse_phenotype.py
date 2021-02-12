448dcc78ad0618d5af886e73cd4fbda372dc55c0
JoelPatchitt <77615720+JoelPatchitt@users.noreply.github.com>
2021-02-12 16:09:34 +0000

    Update parse_phenotype.py

--- bin/parse_phenotype.py
+++ bin/parse_phenotype.py
@@ -8,7 +8,7 @@ import json
 path = Path.cwd()
 file = ("adie-questionnaires_patients.tsv")
 df = os.path.join(path, file)
-output_dir = (path / "results")
+output_dir = (path / "patients_with_subid")
 
 # Load .tsv file into dataframe
 df = pd.read_csv(df, sep='\t')
@@ -21,16 +21,18 @@ sessions = {
     "FY": "oneyear"
 }
 
+
+subid = "participant_id"
 # curate session unrelated labels, pull column names from dataframe
-demographics = ["Age", "GenderID", "GenderBirth", "GenderFit", "Education", "Handedness", "Nationality"]
+demographics = ["participant_id", "Age", "GenderID", "GenderBirth", "GenderFit", "Education", "Handedness", "Nationality"]
 
-height_weight = ["Height", "Weight"]
+height_weight = ["participant_id", "Height", "Weight"]
 
-diagnosis = ["Medication", "PriorDx", "AnxietyDx", "DepressionDx",
+diagnosis = ["participant_id", "Medication", "PriorDx", "AnxietyDx", "DepressionDx",
     "ADHD", "OCD", "PTSD", "CPTSD", "Dyspraxia","Dyslexia", "EatingDisorder",
     "MINIDx", "MINIASCDx", "MINIYes"]
 
-admin = ["Intervention", "Intero_Completion", "PrimOut", "Compliance", "Dropout", "Site"]
+admin = ["participant_id", "Intervention", "Intero_Completion", "PrimOut", "Compliance", "Dropout", "Site"]
 
 assessments = ["BPQ", "TAS", "AQ", "EQ", "STAI",
     "GAD7", "MAIA", "PANAS", "PHQ9", "UCLA", "GSQ"]  # known assessments
@@ -51,7 +53,7 @@ for name, cat in zip(["demographics", "height_weight", "diagnosis", "admin"], # 
     desc = {c: template for c in cat} # [Column name: template] looped for column name in category (c = item in list)
     with open(output_dir / f"{name}.json", "w") as f: # Creates file based on f"{value}" ("w", truncates file) saves as 'f'
         json.dump(desc, f, indent=2) # Dumps desc variable into 'f'.json file 
-    cur_df = df[cat].fillna("n/a") # Fills blank cells in selected category of df with 'n/a'
+    cur_df = df[cat].fillna("n/a").set_index("participant_id") # Fills blank cells in selected category of df with 'n/a'
     cur_df.to_csv(output_dir / f"{name}.tsv", sep="\t") # Saves category in .tsv file based on f'{name}' variable
 
 for k in sessions: # Loops the session keys through the following function:
@@ -59,7 +61,9 @@ for k in sessions: # Loops the session keys through the following function:
         if sess_key == "F":
             for an in assessments: # 'an' = assessment name AKA for item in assessments list
                 sess_key = "F_"
-                subscales = [c for c in df.columns if an in c]  # Identifies columns containing 'an' AKA item in 'assessments'
+                partid = [c for c in df.columns if subid in c]
+                subscales = [c for c in df.columns if an in c]
+                #subscales = [c for c in df.columns if an in c]  # Identifies columns containing 'an' AKA item in 'assessments'
                 subscales = [c for c in subscales if sess_key in c] # Further identifies columns containing the session key 
                 sess_key = "F"
                 if subscales: # If subscales exist run this loop:
@@ -70,11 +74,16 @@ for k in sessions: # Loops the session keys through the following function:
                     with open(output_dir / f"{an}_sess-{sess_key}.json", "w") as f: # Creates file based on f"{value}" ("w", truncates file) saves as 'f'
                         json.dump(desc, f, indent=2) # Dumps 'desc' variable into 'f'.json file 
                         cur_df = df[subscales].fillna("n/a") # Fills blank cells in selected columns off the dataframe with 'n/a'
+                        partid = df[partid].fillna("n/a")
+                        cur_df = cur_df.assign(participant_id=partid)
+                        cur_df = cur_df.set_index("participant_id")
                         cur_df.to_csv(output_dir / f"{an}_sess-F.tsv", sep="\t") # Saves category in .tsv file based on f'{}' variables
         
         else:
             for an in assessments: # 'an' = assessment name AKA for item in assessments list
-                subscales = [c for c in df.columns if an in c]  # Identifies columns containing 'an' AKA item in 'assessments'
+                partid = [c for c in df.columns if subid in c]
+                subscales = [c for c in df.columns if an in c]
+                #subscales = [c for c in df.columns if an in c]  # Identifies columns containing 'an' AKA item in 'assessments'
                 subscales = [c for c in subscales if sess_key in c] # Further identifies columns containing the session key 
                 if subscales: # If subscales exist run this loop:
                     desc = {c.replace(sess_key, ""): template # Removes session indicator for .json file
@@ -84,6 +93,9 @@ for k in sessions: # Loops the session keys through the following function:
                     with open(output_dir / f"{an}_sess-{sess_key}.json", "w") as f: # Creates file based on f"{value}" ("w", truncates file) saves as 'f'
                         json.dump(desc, f, indent=2) # Dumps 'desc' variable into 'f'.json file 
                     
-                    cur_df = df[subscales].fillna("n/a") # Fills blank cells in selected columns off the dataframe with 'n/a'
-                    cur_df.to_csv(output_dir / f"{an}_sess-{sess_key}.tsv", sep="\t") # Saves category in .tsv file based on f'{}' variables
+                        cur_df = df[subscales].fillna("n/a") # Fills blank cells in selected columns off the dataframe with 'n/a'
+                        partid = df[partid].fillna("n/a")
+                        cur_df = cur_df.assign(participant_id=partid)
+                        cur_df = cur_df.set_index("participant_id")
+                        cur_df.to_csv(output_dir / f"{an}_sess-{sess_key}.tsv", sep="\t") # Saves category in .tsv file based on f'{}' variables
     sess_parse(k)
